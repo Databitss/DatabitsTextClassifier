33b9c1090a23cc53cfd75634caf53432ee322084
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2024-12-16 17:56:53 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ def read_requirements(filename):
         return [line.strip() for line in f.readlines() if line.strip() and not line.startswith('#')]
 
 setup(
-    name='databits',  # Ganti dengan nama package kamu
-    version='1.0.3',    # Ganti dengan versi yang sesuai
-    packages=find_packages(),  # Menemukan semua package dalam folder
-    install_requires=read_requirements('requirements.txt'),  # Daftar dependensi yang dibutuhkan
-    long_description=open('README.md').read(),  # Mengambil deskripsi panjang dari README
-    long_description_content_type='text/markdown',  # Tipe konten untuk README
+    name='databits', 
+    version='1.0.3',   
+    packages=find_packages(),  
+    install_requires=read_requirements('requirements.txt'),  
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown', 
     author='Databits Team',
     author_email='databitsteam@gmail.com',
     description='Text Classifier using LSTM, GRU, and Transformer BERT',
-    url='https://github.com/Databitss/databits/',  # URL repositori GitHub kamu
+    url='https://github.com/Databitss/databits/',  
     classifiers=[  
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
