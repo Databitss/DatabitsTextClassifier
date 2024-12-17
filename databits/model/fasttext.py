91122d7b0f056f4504d7f35105bea29d23ef7720
Mfys212 <techpy212@gmail.com>
2024-12-17 23:55:03 +0700

    update

--- databits/model/fasttext.py
+++ databits/model/fasttext.py
@@ -6,7 +6,6 @@ class FASTTEXTModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, output_dim, n_layers, dropout=0.1):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.hidden = nn.Linear(embedding_dim, embedding_dim)
         self.layers = [nn.Linear(embedding_dim, embedding_dim) for _ in range(n_layers)]
         self.fc = nn.Linear(embedding_dim, output_dim)
         self.dropout = nn.Dropout(dropout)
@@ -15,6 +14,6 @@ class FASTTEXTModel(nn.Module):
         embeddings = self.embedding(x)
         avg_embeddings = embeddings.mean(dim=1).squeeze(1)
         for layer in self.layers:
-            avg_embeddings = self.dropout(F.leaky_relu(self.hidden(avg_embeddings)))
+            avg_embeddings = self.dropout(F.leaky_relu(layer(avg_embeddings)))
         out = self.fc(avg_embeddings)
         return out
